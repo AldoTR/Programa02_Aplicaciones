d943905ac98285044b33900d661b62955f7ed59e
zS20006781@estudiantes.uv.mx <zS20006781@estudiantes.uv.mx>
2023-04-23 22:48:16 -0600

    Upload Programa02

--- src/main/java/org/uv/Singleton.java
+++ src/main/java/org/uv/Singleton.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package org.uv.programa02;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author carte
- */
+
 public class Singleton {
     
     private static Singleton s=null;
